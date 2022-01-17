a208779294c85f5fba6f7407b57d7b41cfa819ca
Andrey Demenev <andr2dem@gmail.com>
2022-01-17 17:56:29 +0500

    added template for image rendering

--- python-container/app.py
+++ python-container/app.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, send_file
 from flask_restful import Api, Resource
-import requests
-# from telegram_bot import get_bot
 import telebot
 import telebot.types as tt
 import os
 
-def get_bot(token=None):
+
+def get_bot(token='1694530843:AAHycibEgQoqB0udvbUVn80jCcqUA-vnwnc'):
     token = os.environ.get('TELEGRAM_TOKEN') if token is None else token
     bot = telebot.TeleBot(token=token)
 
@@ -14,34 +13,22 @@ def get_bot(token=None):
     def send_hello(message: tt.Message):
         bot.reply_to(message, f'hello, {message.from_user.full_name}')
 
-    @bot.message_handler(commands=['kill'])
-    def kill_bot(message: tt.Message):
-        bot.reply_to(message, 'killed')
-        bot.stop_polling()
-    
-    @bot.message_handler(commands=['full'])
-    def full_bot(message: tt.Message):
-        bot.reply_to(message, message)
-
     return bot
 
 app = Flask(__name__)
 api = Api(app, prefix="/api/v1")
 bot = get_bot()
 
-class Placeholder(Resource):
-    def get(self):
-        response = requests.get('https://jsonplaceholder.typicode.com/posts/1').json()
-        return response
 
-class SayHello(Resource):
-    def get(self):
-        
-        bot.send_message(chat_id=335645770, text='Кто-то зашёл на сайт!')
-        return 'sent'
+class GetImage(Resource):
+    def get(self, image: str):
+        file = os.path.join(os.curdir, 'images', image)
+        return send_file(file)
 
-api.add_resource(Placeholder, '/placeholder')
-api.add_resource(SayHello, '/hello')
+
+
+api.add_resource(GetImage, '/images/<image>')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
